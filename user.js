0dda52ff15021649c1f323e9234fea474963915b
nathan <57700936+nathanburugu@users.noreply.github.com>
2022-11-15 23:10:24 +0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,48 @@
+/** @format */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+//compare passward
+userSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const userz = mongoose.model('user', userSchema);
+module.exports = userz;
